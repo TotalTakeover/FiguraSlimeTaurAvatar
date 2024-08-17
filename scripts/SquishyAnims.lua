d76941d0d088ad6c44efbfd2f98333b894039696
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-08-17 16:29:10 -0700

    Update SquishyAnims.lua
    - Removed sleeping safeguard from smooth torso offset because it did not do anything
    - Swapped gathered rotation types from `calculateParentRot` function
    - Set pivot of upper body when crouching for better rotations

--- scripts/SquishyAnims.lua
+++ scripts/SquishyAnims.lua
@@ -18,7 +18,7 @@ local function calculateParentRot(m)
 	if not parent then
 		return m:getOffsetRot()
 	end
-	return calculateParentRot(parent) + m:getTrueRot()
+	return calculateParentRot(parent) + m:getOffsetRot()
 	
 end
 
@@ -125,10 +125,15 @@ function events.RENDER(delta, context)
 	slimeParts.LeftArmFP:visible(firstPerson)
 	slimeParts.RightArmFP:visible(firstPerson)
 	
+	-- Set upperbody to offset rot and crouching pivot point
+	slimeParts.UpperBody
+		:rot(-slimeParts.LowerBody:getRot())
+		:offsetPivot(anims.crouch:isPlaying() and -slimeParts.UpperBody:getAnimPos() or 0)
+	
 	-- Offset smooth torso in various parts
-	-- Note: acts strangely with `slimeParts.body` and when sleeping
+	-- Note: acts strangely with `slimeParts.body`
 	for _, group in ipairs(slimeParts.UpperBody:getChildren()) do
-		if group ~= slimeParts.Body and not pose.sleep then
+		if group ~= slimeParts.Body then
 			group:rot(-calculateParentRot(group:getParent()))
 		end
 	end
